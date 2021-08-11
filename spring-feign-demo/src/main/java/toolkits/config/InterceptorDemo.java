825ea748d820067190ab6bbf9cdc6622c0696561
dominiczhu <dominiczhu@tencent.com>
2021-08-11 11:38:12 +0800

    value

--- spring-feign-demo/src/main/java/toolkits/config/InterceptorDemo.java
+++ spring-feign-demo/src/main/java/toolkits/config/InterceptorDemo.java
@@ -2,6 +2,8 @@ package toolkits.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 /**
  * @author dominiczhu
@@ -9,9 +11,16 @@ import feign.RequestTemplate;
  * @title InterceptorDemo
  * @date 2021/8/11 上午11:05
  */
+// 不可以加component，否则这个拦截器会全局生效
+//@Component
 public class InterceptorDemo implements RequestInterceptor {
+
+    // 但是即使没有component，value注解也会生效
+    @Value("${header-test}")
+    private String h;
+
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        requestTemplate.header("header-test","test");
+        requestTemplate.header("header-test",h);
     }
 }
